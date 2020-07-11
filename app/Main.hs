d1cb6e729d11e8140d0c921dbb518df6377fe655
Rose <rose@empty.town>
2020-07-11 14:59:58 +0100

    Added a back buffer to improve performance

--- app/Main.hs
+++ app/Main.hs
@@ -12,11 +12,12 @@ import Control.Monad.Reader   (ReaderT, ask, local, runReaderT)
 import Data.Bifunctor         (first, second)
 import Data.Bits              ((.|.))
 import Data.Int               (Int32)
+import Data.Ord               (comparing)
 import Numeric                (readHex)
 import Options.Applicative
     (Parser, execParser, help, helper, info, long, maybeReader, option, short,
     strOption, value, (<**>))
-import Safe                   (headMay)
+import Safe                   (headMay, maximumByDef)
 import System.Environment     (lookupEnv)
 
 import qualified Graphics.X11 as X
@@ -41,6 +42,7 @@ data Environment = Environment
   , font         :: X.FontStruct
   , scrollOffset :: (X.Position, X.Position)
   , theme        :: (X.Pixel, X.Pixel)
+  , gc           :: X.GC
   }
 
 data PositionedText = PositionedText
@@ -55,14 +57,23 @@ main = do
   display      <- maybe (error "Couldn't open display!") X.openDisplay =<< lookupEnv "DISPLAY"
   font         <- X.loadQueryFont display fontName
   content      <- lines <$> getContents
-  root         <- X.rootWindow display 0
+  root         <- X.rootWindow display $ X.defaultScreen display
   window       <- X.createSimpleWindow display root 0 0 250 250 0 background background
   scrollOffset <- pure (0, 0)
   theme        <- pure (foreground, background)
+  gc           <- X.createGC display window
+
+  X.setForeground display gc $ fst theme
+  X.setBackground display gc $ snd theme
+  X.setFont display gc $ X.fontFromFontStruct font
+
   X.selectInput display window $ X.exposureMask .|. X.buttonPressMask
   X.mapWindow display window
-  runReaderT eventLoop Environment {..}
+
+  runReaderT (withPixmap eventLoop) Environment {..}
+
   X.freeFont display font
+  X.freeGC display gc
 
 readPixel :: String -> Maybe X.Pixel
 readPixel ('#':xs) = readPixel  xs
@@ -76,26 +87,29 @@ optionParser = do
   background <- option (maybeReader readPixel) (long "background" <> short 'b' <> help "background color, hex" <> value defaultBG)
   pure Arguments {..}
 
-eventLoop :: ReaderT Environment IO ()
-eventLoop = do
+eventLoop :: X.Pixmap -> ReaderT Environment IO ()
+eventLoop source = do
   Environment {..} <- ask
   event <- liftIO $ X.allocaXEvent $
     \event -> X.nextEvent display event >> pure event
 
   liftIO $ do
-    drawLines display window (fst theme) font scrollOffset content
-    X.sync display False
+    (_, _, _, width, height, _, _) <- X.getGeometry display window
+    let (x, y) = scrollOffset
+    X.clearWindow display window
+    X.copyArea display source window gc x y width height 0 0
+    X.sync display True
 
   maybeButton <- liftIO $ X.get_EventType event >>= \type_ -> if
     | type_ == X.buttonPress -> Just <$> eventButton <$> X.get_ButtonEvent event
     | otherwise              -> pure Nothing
 
   case maybeButton of
-    Just 4 -> local (\e -> e { scrollOffset = second (+ 10) scrollOffset }) eventLoop
-    Just 5 -> local (\e -> e { scrollOffset = second (subtract 10) scrollOffset }) eventLoop
-    Just 6 -> local (\e -> e { scrollOffset = first (+ 10) scrollOffset }) eventLoop
-    Just 7 -> local (\e -> e { scrollOffset = first (subtract 10) scrollOffset }) eventLoop
-    _      -> eventLoop
+    Just 4 -> local (\e -> e { scrollOffset = second (+ 10) scrollOffset }) $ eventLoop source
+    Just 5 -> local (\e -> e { scrollOffset = second (subtract 10) scrollOffset }) $ eventLoop source
+    Just 6 -> local (\e -> e { scrollOffset = first (+ 10) scrollOffset }) $ eventLoop source
+    Just 7 -> local (\e -> e { scrollOffset = first (subtract 10) scrollOffset }) $ eventLoop source
+    _      -> eventLoop source
 
 eventButton :: X.XButtonEvent -> X.Button
 eventButton (_, _, _, _, _, _, _, _, button, _) = button
@@ -110,18 +124,30 @@ characterHeight :: X.FontStruct -> Int32
 characterHeight = uncurry (+) . fontMetrics
 
 -- arrange a list of strings vertically according to the font size
-verticalize :: [String] -> X.FontStruct -> X.Position -> [PositionedText]
-verticalize texts font origin = zipWith arrange [0..] texts
+verticalize :: [String] -> X.FontStruct -> [PositionedText]
+verticalize texts font = zipWith arrange [1..] texts
   where
-    arrange index text = PositionedText text (origin + index * characterHeight font) $ X.textWidth font text
+    arrange index text = PositionedText text (index * characterHeight font) $ X.textWidth font text
 
-drawLines :: X.Display -> X.Window -> X.Pixel -> X.FontStruct -> (X.Position, X.Position) -> [String] -> IO ()
-drawLines display window fgColor font origins texts = do
-  gc   <- X.createGC display window
-  let textPositions = verticalize texts font $ snd origins + characterHeight font
-  X.clearWindow display window
-  X.setForeground display gc fgColor
-  X.setFont display gc $ X.fontFromFontStruct font
-  forM_ textPositions $ \PositionedText {..} ->
-    X.drawString display window gc (fst origins) ptY ptText
-  X.freeGC display gc
+withPixmap :: (X.Pixmap -> ReaderT Environment IO a) -> ReaderT Environment IO a
+withPixmap actionM = do
+  Environment {..} <- ask
+  let height = fromIntegral $ characterHeight font * fromIntegral (length content)
+  let width = fromIntegral $ X.textWidth font $ maximumByDef "" (comparing length) content
+  pixmap <- liftIO $ X.createPixmap display window width height $ X.defaultDepth display $ X.defaultScreen display
+  liftIO $ do
+    X.setForeground display gc $ snd theme
+    X.fillRectangle display pixmap gc 0 0 width height
+    X.setForeground display gc $ fst theme
+  renderContent pixmap
+  result <- actionM pixmap
+  liftIO $ X.freePixmap display pixmap
+  pure result
+
+renderContent :: X.Pixmap -> ReaderT Environment IO ()
+renderContent target = do
+  Environment {..} <- ask
+  liftIO $ do
+    let textPositions = verticalize content font
+    forM_ textPositions $ \PositionedText {..} ->
+      X.drawString display target gc (fst scrollOffset) ptY ptText
